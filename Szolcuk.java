a31825c99181ce020ecc2cb6ff68158b4492e123
csengekulker <k71608883429@ktch.hu>
2022-02-21 18:14:05 +0100

    printing and scanning data

--- Szolcuk.java
+++ Szolcuk.java
@@ -7,3 +7,22 @@
 * Github: https://github.com/csengekulker/
 * Licenc: GNU GPL
 */
+
+import java.util.Scanner;
+
+class Szolcuk {
+    public static void main(String [] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("\tBalogh Csenge\n\tSzoft_I_N\n\t2022.02.21");
+
+        System.out.print("Széntartalom > ");
+        int coal = sc.nextInt();
+        System.out.print("Hidrogéntartalom > ");
+        int hydrogen = sc.nextInt();
+        System.out.print("Oxigéntartalom > ");
+        int oxygen = sc.nextInt();
+
+        sc.close();
+    }
+}
